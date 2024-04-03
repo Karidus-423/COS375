d5937e692939026ddac1c581d1aa7eabb49ffa19
Karidus-423 <kapuerto23@gmail.com>
2024-04-03 15:01:33 -0400

    asaber

--- Apple-Picker/scripts/player.gd
+++ Apple-Picker/scripts/player.gd
@@ -9,38 +9,38 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
-    # Add the gravity.
-    if not is_on_floor():
-        velocity.y += gravity * delta
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
 
-    # Handle jump.
-    if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-        velocity.y = JUMP_VELOCITY
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
 
-    # Get the input direction and handle the movement/deceleration.
-    # As good practice, you should replace UI actions with custom gameplay actions.
-    var direction = Input.get_axis("ui_left", "ui_right")
-    if direction:
-        velocity.x = direction * SPEED
-    else:
-        velocity.x = move_toward(velocity.x, 0, SPEED)
-
-
-    for index in range(get_slide_collision_count()):
-        var collision = get_slide_collision(index)
-
-        if collision.get_collider() == null:
-            continue
-
-        if collision.get_collider().is_in_group("apple"):
-            var apple = collision.get_collider()
-
-            apple.collect()
-            break
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 
-    move_and_slide()
+	for index in range(get_slide_collision_count()):
+		var collision = get_slide_collision(index)
+
+		if collision.get_collider() == null:
+			continue
+
+		if collision.get_collider().is_in_group("apple"):
+			var apple = collision.get_collider()
+
+			apple.collect()
+			break
+
+
+	move_and_slide()
 
 
 func _on_lives_label_death():
-    queue_free()
+	queue_free()
--- Mission-Demolition/scripts/player.gd
+++ Mission-Demolition/scripts/player.gd
@@ -1,28 +1,10 @@
 extends CharacterBody2D
 
-var bomb_pos : Vector2
-var mouse_down: bool
-var has_started: bool
+var Bomb = load("res://scenes/cannon.tscn")
 
-var test_cannon = preload("res://scenes/cannon.tscn")
-
-func _ready():
-    bomb_pos = $Player.global_position
-    has_started = false
-
-func _process(delta):
-    mouse_down = Input.is_action_pressed("MouseDown")
-    if(mouse_down):
-        has_started = true
-        if(position.distance_to(get_global_mouse_position()) < 100):
-            $test_cannon.global_position = get_global_mouse_position()
-        else:
-            $test_cannon.global_position = bomb_pos + (get_global_mouse_position()-position).normalized()*100
-            
-    elif(has_started):
-        _shoot()
-        has_started = false
-
-
-func _shoot():
-    print("shot")
+func _unhandled_input(event):
+	if event is InputEventMouseButton and event.pressed:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			var b = Bomb.instantiate()
+			b.position = event.position
+			add_child(b)
