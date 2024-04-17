b28bd7c91f19c784bfc5f7dfbc778670d603d914
Karidus-423 <kapuerto23@gmail.com>
2024-04-16 23:38:57 -0400

    Shooting **

--- Space-SCHMUP/main/main.gd
+++ Space-SCHMUP/main/main.gd
@@ -16,11 +16,8 @@ func _on_enemy_timer_timeout() -> void:
 	# Choose a random location on Path2D.
 	var scout_spawn_location = $SpawnPath/SpawnLocation
 	scout_spawn_location.progress_ratio = randf()
-
-
 	# Set the scout's position to a random location.
 	scout.position = scout_spawn_location.position
-
 	# Choose the velocity for the scout.
 	scout.velocity = Vector2(0,speed)
 	add_child(scout)
