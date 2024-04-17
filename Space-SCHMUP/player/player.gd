1ace3ee5c8b96075ddbaa36bfedd1fd684430d68
Karidus-423 <kapuerto23@gmail.com>
2024-04-16 21:00:15 -0400

    testing on windows

--- Space-SCHMUP/player/player.gd
+++ Space-SCHMUP/player/player.gd
@@ -7,14 +7,24 @@ extends CharacterBody2D
 
 var input = Vector2.ZERO
 
+@onready var shield_animation = $ShieldAnimations/AnimationPlayer
+
 func _physics_process(delta):
 	player_movement(delta)
 	player_animation()
 
+
+### Player Input
+func direction_value(movement_key_pressed) -> int:
+	var direction  = int(Input.is_action_pressed(movement_key_pressed))
+	return direction
+
 func get_input():
-	input.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
-	input.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
+	input.x =  direction_value("ui_right") - direction_value("ui_left") 
+	input.y =  direction_value("ui_down") - direction_value("ui_up")
+
 	return input.normalized()
+
 
 func player_movement(delta):
 	input = get_input()
@@ -31,11 +41,11 @@ func player_movement(delta):
 	move_and_slide()
 
 
-func player_animation():
+func player_animation() -> void:
 	if Input.is_action_pressed("ui_right") == true:
 		$PlayerAnimation.play("move_right")
 	elif Input.is_action_pressed("ui_left") == true:
 		$PlayerAnimation.play("move_left")
 	else:
 		$PlayerAnimation.play("idle")
-		
+		shield_animation.play("tier-1")
